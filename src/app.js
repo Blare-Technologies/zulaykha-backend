bb0375356e110d3c0dd60c54b8bb91a074c946d1
blareTech01 <147812631+blareTech01@users.noreply.github.com>
2025-12-20 18:07:27 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,15 +24,15 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        console.log(origin);
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    // origin: '*',
+    // origin: function (origin, callback) {
+    //     console.log(origin);
+    //     if (!origin || allowedOrigins.includes(origin)) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error("Not allowed by CORS"));
+    //     }
+    // },
+    origin: '*',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
