c987fc87429308131d102894ce9582bc2cc8bc0a
blareTech01 <147812631+blareTech01@users.noreply.github.com>
2025-12-20 18:35:12 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ const corsOptions = {
         }
     },
     // origin: '*',
-    // credentials: true,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
